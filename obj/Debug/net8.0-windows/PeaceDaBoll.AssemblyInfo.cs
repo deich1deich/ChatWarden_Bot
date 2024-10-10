ebf3b14b1db2acbdbca02f6aaa45bfffc397de33
PavelNikolaets <pavel.nikolaets@yandex.ru>
2024-10-10 23:47:21 +0300

    branch

--- obj/Debug/net8.0-windows/PeaceDaBoll.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/PeaceDaBoll.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PeaceDaBoll")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0fb285107242cef5539a506273a7a803829d5918")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1d7e29f77e8158bb819a697d756223d26abf41b")]
 [assembly: System.Reflection.AssemblyProductAttribute("PeaceDaBoll")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PeaceDaBoll")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
